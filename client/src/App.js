48bf6eae4f53d72088a108b7cdf1ae6e4ce945e4
georges <georgesazzi22@hotmail.com>
2022-06-24 21:14:43 +0300

    Creating React App

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        h1 > Employee Application < /h1>  <
+        /div>
+    );
+}
+
+export default App;
