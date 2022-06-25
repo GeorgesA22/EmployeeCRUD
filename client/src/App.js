d9b036d8b4ab6870dfd9a91cb0aa6a718ddf574c
georges <georgesazzi22@hotmail.com>
2022-06-25 04:11:32 +0300

    Initializing server side

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
     return ( <
         div className = "App" >
         <
-        h1 > Employee Application < /h1>  <
+        h1 > Employee Application < /h1>   <
         /div>
     );
 }
